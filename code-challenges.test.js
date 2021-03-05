3cfba298a16cb887db27823717989283e6dcbcde
algtan <agtan@ymail.com>
2021-03-05 15:52:40 -0800

    completed JS and Ruby code challenges

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -18,18 +18,47 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
-var colors1 = ["purple", "blue", "green", "yellow", "pink"]
+// var colors1 = ["purple", "blue", "green", "yellow", "pink"]
 // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-var colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+// var colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
 // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
 
+describe("shuffleRemainingItems", () => {
+    it("return array with first item removed and the remaining items shuffled", () => {
+        const colors1 = ["purple", "blue", "green", "yellow", "pink"];
+        const colors1Expected = ["yellow", "blue", "pink", "green"];
+        const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"];
+        const colors2Expected = ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"];
 
-
+        expect( shuffleRemainingItems(colors1).sort() ).toEqual( colors1Expected.sort() );
+        expect( shuffleRemainingItems(colors2).sort() ).toEqual( colors2Expected.sort() );
+    })
+})
 
 
 // b) Create the function that makes the test pass.
 
-
+// Create a function that takes in 1 argument that is an array
+const shuffleRemainingItems = (array) => {
+    // Remove the first element from the array
+    array.shift();
+    // Shuffle the remaining elements in the array
+    // Iterate through the smaller array, working backwards to shuffle the items
+    for (let i = array.length - 1; i > 0; i--) {
+        // Create a variable 'j' that will be used to choose a random index from the subarray of unshuffled indices
+        // Initially, 'j' will be a random number from 0 to 3 for a smallerArray length of 4
+        let j = Math.floor(Math.random() * (i + 1));
+        // Create a temporary variable to store the value of the last element (index 'i') of the unshuffled subarray
+        let temp = array[i];
+        // Swap the value at the last element (index 'i') of the unshuffled subarray with a randomly chosen element in the unshuffled subarray (index 0 to index 'i')
+        // NOTE: It could be swapped with itself
+        array[i] = array[j]
+        // Store the initial unshuffled element at index 'i', now in temp, into index 'j'
+        array[j] = temp;
+    }
+    // Return shuffled array
+    return array;
+}
 
 
 
@@ -42,14 +71,36 @@ var cubeAndSum1 = [2, 3, 4]
 var cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
-
-
+describe("sumOfAllCubed", () => {
+    it("return a number that is the sum of the array's elements cubed", () => {
+        const cubeAndSum1 = [2, 3, 4]
+        const cubeAndSum2 = [0, 5, 10]
+        expect(sumOfAllCubed(cubeAndSum1)).toEqual(99);
+        expect(sumOfAllCubed(cubeAndSum2)).toEqual(1125);
+    })
+})
 
 
 // b) Create the function that makes the test pass.
 
+// Create a function that takes in 1 argument that is an array
+const sumOfAllCubed = (array) => {
+    // Create a new array that stores the cube of the initial array's values
+    cubedArray = array.map(value => {
+        return value ** 3;
+    })
 
+    // Use an accumulator to go through the cubedArray to sum all the elements
+    let accumulator = 0;
 
+    // Iterate through the cubedArray to get the sum
+    cubedArray.forEach(value => {
+        accumulator += value;
+    });
+    
+    // Return a number that is the sum of all the elements cubed
+    return accumulator;
+}
 
 
 // --------------------3) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
@@ -61,13 +112,41 @@ var nums1 = [3, 56, 90, -8, 0, 23, 6]
 var nums2 = [109, 5, 9, -59, 8, 24]
 // Expected output: [-59, 109]
 
-
-
+describe("minAndMax", () => {
+    it("return an array that contains the minimum and maximum of the input array", () => {
+        const nums1 = [3, 56, 90, -8, 0, 23, 6];
+        const nums1Expected = [-8, 90];
+        const nums2 = [109, 5, 9, -59, 8, 24];
+        const nums2Expected = [-59, 109];
+        expect(minAndMax(nums1)).toEqual(nums1Expected);
+        expect(minAndMax(nums2)).toEqual(nums2Expected);
+    })
+})
 
 
 // b) Create the function that makes the test pass.
 
+// Create a function that takes in 1 argument that is an array
+const minAndMax = (array) => {
+    // Create a variable to store the minimum and set it initially to the first element of the array
+    let min = array[0];
+    // Create a variable to store the maximum and set it initially to the first element of the array
+    let max = array[0];
 
+    // Iterate through the array and see if an element is smaller than the current minimum, or larger than the current maximum
+    array.forEach(value => {
+        // Update the minimum if the value is smaller than the current minimum
+        if (value < min) {
+            min = value;
+        // Update the maximum if the value is larger than the current maximum
+        } else if (value > max) {
+            max = value;
+        }
+    })
+    
+    // Return an array that contains the minimum and the maximum values of the input array
+    return [min, max];
+}
 
 
 
@@ -80,14 +159,40 @@ var testString1 = "albatross"
 var testString2 = "jabberwocky"
 // Expected output: "jAbBeRwOcKy"
 
-
-
+describe("alternatingCaps", () => {
+    it("return a string that alternates capital letters for an input string", () => {
+        const testString1 = "albatross";
+        const testString1Expected = "aLbAtRoSs";
+        const testString2 = "jabberwocky";
+        const testString2Expected = "jAbBeRwOcKy";
+        expect(alternatingCaps(testString1)).toEqual(testString1Expected);
+        expect(alternatingCaps(testString2)).toEqual(testString2Expected);
+    })
+})
 
 
 // b) Create the function that makes the test pass.
 
+// Create a function that takes in 1 argument that is a string
+const alternatingCaps = (string) => {
+    // Split the string into an array
+    strArray = string.split('');
+    // Iterate through the array in which we capitalize the letter based on its index
+    alternatingCapArray = strArray.map((value, index) => {
+        // Check if the index is odd using the modulo
+        if (index % 2 != 0) {
+            // Capitalize the letter if it is an odd index
+            return value.toUpperCase();
+        // Otherwise do nothing to the value
+        } else {
+            return value;
+        }
+    })
 
-
+    // Join the array back together into a string
+    // Return a string with alternating capitalization
+    return alternatingCapArray.join('');
+}
 
 
 // --------------------5) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
@@ -98,8 +203,35 @@ var testArray1 = [3, 7, 10, 5, 4, 3, 3]
 var testArray2 = [7, 8, 2, 3, 1, 5, 4]
 // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
 
-
-
+describe("noDuplicateNums", () => {
+    it("return an array with no duplciate values from an input of two arrays", () => {
+        const testArray1 = [3, 7, 10, 5, 4, 3, 3];
+        const testArray2 = [7, 8, 2, 3, 1, 5, 4];
+        const expectedArrFromTestArrs = [3, 7, 10, 5, 4, 8, 2, 1];
+        expect( noDuplicateNums(testArray1, testArray2) ).toEqual(expectedArrFromTestArrs);
+    })
+})
 
 
 // b) Create the function that makes the test pass.
+
+// Create a function that takes in two arguments, both of which are arrays
+const noDuplicateNums = (array1, array2) => {
+    // Create an empty array that will store the unique values
+    uniqueArr = [];
+
+    // Join the two input arrays together into one array
+    joinedArr = array1.concat(array2);
+    
+    // Iterate through the joined arrays, adding the unique values to the new array
+    joinedArr.forEach(value => {
+        // Check if the new array does not already contain the value
+        if (!uniqueArr.includes(value)) {
+            // If it doesn't add the value to that array
+            uniqueArr.push(value);
+        }
+    })
+
+    // Return a single array with no duplicate values
+    return uniqueArr;
+}
--- code_challenges.rb
+++ code_challenges.rb
@@ -8,8 +8,19 @@ fullArr1 = [4, 9, 0, '7', 8, true, 'hey', 7, 199, -9, false, 'hola']
 fullArr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
 # Expected output: [-823, 7, 23]
 
+# Create a method that takes in array of mixed data
+def oddNums array
+    # Go through the array and select only the integers
+    numArray = array.select { |value| value.is_a?(Integer) }
+    # Go through the array of integers and select the ones that are odd
+    oddNumArray = numArray.select { |value| value.odd? }
+    # Sort the array of odd numbers in place using the bang operator
+    oddNumArray.sort!
+    # Implicity return the array of odd numbers, sorted from least to greatest
+end
 
-
+p oddNums fullArr1
+p oddNums fullArr2
 
 
 # --------------------2) Create a method that takes in an array of words and a letter and returns all the words that contain that particular letter.
@@ -20,8 +31,18 @@ letter_o = 'o'
 letter_a = 'a'
 # Expected output: ['tea', 'soda water']
 
+# Create a method that takes in 2 arguments
+# arg1 = array of words
+# arg2 = letter to check for (string)
+def containLetter(array, letter)
+    # Iterate through the array of words
+    # Use select to check if that letter is in the word
+    selected_array = array.select { |value| value.include?(letter) }
+    # Implicity return an array of words that contain that letter with the statement above
+end
 
-
+p containLetter(beverages_array, letter_o)
+p containLetter(beverages_array, letter_a)
 
 
 # -------------------3) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
@@ -33,19 +54,71 @@ album2 = 'Sgt Pepper'
 album3 = 'Abbey Road'
 # Expected output: 'bby Rd'
 
+# Create method that takes in 1 argument that is a string
+def noVowels string
+    # # Create a string that contains all the vowels
+    # vowels = 'aeiou'
+    # # Split the string into an array
+    # strArray = string.split('')
+    # # Use 'select' to only keep the characters that are not vowels
+    # consonantArray = strArray.select { |value| !vowels.include?(value.downcase) }
+    # # Join the array into a string
+    # consonantsOnly = consonantArray.join('')
 
+    #Use the delete method that checks lowercase vowels and uppercase vowels
+    string.delete "aeiouAEIOU"
+    # Implicity return a string with all the vowels removed with the line above
+end
 
+p noVowels album1
+p noVowels album2
+p noVowels album3
 
 
 # --------------------4) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a get_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
+class Bike
+    attr_accessor :model, :wheels, :current_speed
 
+    def initialize model
+        @model = model
+        @wheels = 2
+        @current_speed = 0
+    end
 
+    def get_info
+        "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph."
+    end
+
+    def pedal_faster increase_in_speed
+        @current_speed = @current_speed + increase_in_speed
+    end
+
+    def brake decrease_in_speed
+        @current_speed = @current_speed - decrease_in_speed
+        if @current_speed < 0
+            @current_speed = 0
+        end
+    end
+end
 
 
 # -------------------5) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed. The brake method should decrease the speed. The bike cannot go negative speeds.
 
 # Expected output example: my_bike.pedal_faster 10 => 10
 # Expected output example: my_bike.brake 15 => 0
+
+# Create a new Bike object
+my_bike = Bike.new "Trek"
+
+# Increase the speed
+my_bike.pedal_faster 10
+# Get information of bike
+p my_bike.get_info
+
+# Increase the speed
+my_bike.brake 15
+# Get information of bike
+p my_bike.get_info
